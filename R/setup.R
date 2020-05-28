a0489a2ec0fc48ff4323e0fe6b955cfec4ac40a3
rafapereirabr <rafa.pereira.br@gmail.com>
2020-05-27 22:44:34 -0300

    1st push

---
+++ R/setup.R
@@ -0,0 +1,42 @@
+Sys.setenv(TZ='UTC') # Fuso horario local
+
+# carregar bibliotecas
+library(ggplot2)      # visualizacao de dados
+library(ggthemes)     # temas para visualizacao de dados
+library(sf)           # leitura e manipulacao de dados espaciais
+library(data.table)   # manipulacao de dados
+library(geobr)        # dados espaciais do brasil
+library(pbapply)      # progress bar
+library(readr)        # rapida leitura de dados 
+library(tidyr)        # manipulacao de dados
+library(stringr)      # operacoes em strings
+library(lubridate)    # dados em data/horario
+library(mapview)      # visualizacao interativa dos dados
+library(RColorBrewer) # paleta de cores
+# library(extrafont)    # fontes de texto
+library(furrr)
+library(purrr)
+library(dplyr)
+library(hrbrthemes)
+library(beepr)
+
+# disable scientific notation
+options(scipen=10000)
+
+
+# Use GForce Optimisations in data.table operations
+# details > https://jangorecki.gitlab.io/data.cube/library/data.table/html/datatable-optimize.html
+options(datatable.optimize=Inf)
+
+# set number of threads used in data.table
+data.table::setDTthreads(percent = 100)
+
+
+
+## usefull support functions
+
+`%nin%` = Negate(`%in%`)
+`%nlike%` = Negate(`%like%`)
+
+
+  
