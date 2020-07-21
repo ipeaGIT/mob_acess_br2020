b668e32e44247dbb67907e6c262a8fadec29bea3
Joao Bazzo <joao.bazzo@gmail.com>
2020-07-21 11:38:16 -0300

    add jan data analysis

--- R/DENATRAN/1-download-DENATRAN.R
+++ R/DENATRAN/1-download-DENATRAN.R
@@ -22,7 +22,7 @@ unlink(x = files_rm,recursive = FALSE,force = TRUE)
 
 
 #
-# 2000 - 2012 DENATRAN FILES ---------------
+# 1999 - 2012 DENATRAN FILES ---------------
 #
 
 html <- paste(readLines(link), collapse="\n")
@@ -49,15 +49,44 @@ lapply(seq_along(temp_zip), function(i){
   
 })
 
-temp_html <- temp_matched[temp_matched %like% ".html"] # 2013-2019
+#
+# 2020 ----
+#
+
+link2020 <- "/component/content/article/115-portal-denatran/9484"
+html2020 <- paste(readLines(paste0(body,link2020)), collapse="\n")
+temp_2020 <- stringr::str_match_all(html2020, "<a href=\"(.*?)\" style")[[1]][,2]
+# only january
+temp_2020 <- temp_2020[temp_2020 %like% ".xlsx" |
+                         temp_2020 %like% ".xls"]
+temp_2020 <- temp_2020[temp_2020 %like% "jan" | 
+                         temp_2020 %like% "JAN" |
+                         temp_2020 %like% "Jan"]
+names_file <- stringr::str_split(temp_2020,"\\/") 
+names_file <- do.call(cbind,names_file)
+names_file <- names_file[nrow(names_file),]
+# create dir
+suppressWarnings(dir.create("data-raw/DENATRAN/2020"))
+# download files and save
+lapply(seq_along(temp_2020), function(j){ # j = 1
+  
+  message(names_file[j]) 
+  
+  aux_dest_file <- paste0("data-raw/DENATRAN/2020/",names_file[j])
+  download.file(url = paste0(body,temp_2020[j]),
+                destfile = aux_dest_file)
+  
+})
+
 
 #
 # new years (2013-2019) -------------------
 #
 
-years <- 2020:2013
+temp_html <- temp_matched[temp_matched %like% ".html"] # 2013-2019
+years <- 2019:2013
 
-for(i in seq_along(years)){ # i = 1
+for(i in seq_along(years)){ # i = 4
   message(years[i])
   
   # complete link
@@ -65,14 +94,24 @@ for(i in seq_along(years)){ # i = 1
   temp_url <- paste(readLines(temp_url,encoding = "UTF-8"), collapse="\n")
   matched <- stringr::str_match_all(temp_url, "<a href=\"(.*?)\" style")[[1]][,2]
   temp_xlsx <- matched[matched  %like% ".xlsx" | 
-                         matched  %like% ".xls" | 
-                         matched %like% ".rar" | 
-                         matched %like% ".zip"]
+                         matched  %like% ".xls" ]
+  # only january
+  temp_xlsx <- temp_xlsx[temp_xlsx %like% "JAN" | 
+                           temp_xlsx %like% "jan" | 
+                           temp_xlsx %like% "Jan" ]
   
-  # only december
-  temp_xlsx <- temp_xlsx[temp_xlsx %like% "DEZ" | 
-                           temp_xlsx %like% "dez" | 
-                           temp_xlsx %like% "Dez" ]
+  # check if has .rar files
+  if(length(temp_xlsx) == 0){
+    
+    temp_xlsx <- matched[matched %like% ".rar" |
+                           matched %like% ".zip"]
+    # only january
+    temp_xlsx <- temp_xlsx[temp_xlsx %like% "JAN" | 
+                             temp_xlsx %like% "jan" | 
+                             temp_xlsx %like% "Jan" ]
+  }
+  
+
   
   # namesfiles
   names_file <- stringr::str_split(temp_xlsx,"\\/") 
@@ -97,22 +136,33 @@ for(i in seq_along(years)){ # i = 1
 #
 # extract all files-----
 #
+# There are zip within zips, so this part of script has 
+# to be executed twice
+#
+#
 
 allfiles <- list.files(path = "data-raw/DENATRAN",all.files = TRUE,recursive = TRUE,
                        full.names = TRUE)
 allfiles <- allfiles[allfiles %like% ".rar" | allfiles %like% ".zip"]
+
+# these file has to be executed manually (dont know why)
+
+exclude2015 <- allfiles[allfiles %like% "7_Frota_UF_Municipio_Marca_Modelo_JAN-15.rar" |
+                          allfiles %like% "Frota_por_Municipio_e_Tipo_JAN_15.rar"]
+allfiles <- allfiles[allfiles %nin% exclude2015]
+
+# names
 names_file <- stringr::str_split(allfiles,"\\/") 
 names_file <- do.call(cbind,names_file) %>% t() %>% data.table::as.data.table()
 names_file[,foldernames := paste0(V1,"/",V2,"/",V3,"/")]
 names_file[,allfiles := allfiles]
 
-lapply(66:nrow(names_file),function(i){ # i = 38
+lapply(1:nrow(names_file),function(i){ # i = 38
   message(names_file$allfiles[i])
   if(names_file$V4[i] %like% ".zip"){
     unzip(zipfile = names_file$allfiles[i],exdir = names_file$foldernames[i])
-  }
- 
-  if(names_file$V4[i] %like% ".rar"){
-    archive::archive_extract(archive = names_file$allfiles[i],dir = names_file$foldernames[i])
+  }else{ # .rar 
+    archive::archive_extract(archive = names_file$allfiles[i],
+                             dir = names_file$foldernames[i])
   }
 })
